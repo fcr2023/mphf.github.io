7bab7d279448a8aa35a3c46ad91f443874198994
Fredson Ribeiro <fredsonet@gmail.com>
2022-10-14 17:48:20 -0400

    implements new user screen

--- src/app/account/new-account-component.ts
+++ src/app/account/new-account-component.ts
@@ -1,13 +1,23 @@
 import { Component, VERSION } from '@angular/core';
 import {
   AbstractControl,
+  ControlContainer,
   FormBuilder,
   FormControl,
   FormGroup,
+  FormGroupDirective,
+  NgForm,
   Validators,
 } from '@angular/forms';
 import { Location } from '@angular/common';
+import { ErrorStateMatcher } from '@angular/material/core';
 
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 @Component({
   selector: 'new-account',
   templateUrl: './new-account-component.html',
@@ -18,12 +28,25 @@ export class AppNewAccount {
   passwordsMatching = false;
   isConfirmPasswordDirty = false;
   confirmPasswordClass = 'form-control';
+  
+  user: string = '';
+  pwd: string = '';
+  pwdconf: string = '';
+
+  hide = true;
+  hide2 = true;
+
+  matcher = new MyErrorStateMatcher();
+    
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
   newPassword = new FormControl(null, [
     (c: AbstractControl) => Validators.required(c),
     Validators.pattern(
       /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/
     ),
   ]);
+
   confirmPassword = new FormControl(null, [
     (c: AbstractControl) => Validators.required(c),
     Validators.pattern(
@@ -31,7 +54,9 @@ export class AppNewAccount {
     ),
   ]);
 
-  resetPasswordForm = this.formBuilder.group(
+  // userName = new FormControl(null, [() => {}]);
+
+  registerForm = this.formBuilder.group(
     {
       newPassword: this.newPassword,
       confirmPassword: this.confirmPassword,
@@ -47,8 +72,10 @@ export class AppNewAccount {
     ) {}
 
   onSubmit(): void {
-    console.log(this.resetPasswordForm);
-    if (!this.resetPasswordForm?.valid) {
+    console.log(this.user);
+    console.log(this.pwd);
+    console.log(this.registerForm);
+    if (!this.registerForm?.valid) {
       return;
     }
   }
